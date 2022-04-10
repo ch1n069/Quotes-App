14f4a89ecc3d230398122d0f90582603bc400058
Moringa School <brunolalpachino@gmail.com>
2022-04-10 08:35:15 +0300

    changes in the quotes component

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
 
+  public quotes: Quotez [] = [
+    new Quotez(1,)
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
