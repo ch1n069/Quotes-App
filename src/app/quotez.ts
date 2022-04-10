6f4a43bffadb7b5c1d7ea014c6dd33fffa870188
Moringa School <brunolalpachino@gmail.com>
2022-04-10 08:34:38 +0300

    created quotez class constructor

---
+++ src/app/quotez.ts
@@ -0,0 +1,17 @@
+
+export class Quotez {
+    id:number;
+    quote:string;
+    author:string;
+    upvotes:number;
+    downvotes:number;
+
+    constructor(id:number , quote:string, author:string , upvotes:number, downvotes:number){
+        this.id = id;
+        this.quote = quote;
+        this.author = author; 
+        this.upvotes = upvotes;
+        this.downvotes = downvotes;
+
+    }
+}
