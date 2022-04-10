f8021ddd79f45f1036663147d6030b35a647b675
Moringa School <brunolalpachino@gmail.com>
2022-04-10 21:08:31 +0300

    added the quote form component

---
+++ src/app/quote-form/quote-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Quotez } from '../quotez';
+
+@Component({
+  selector: 'app-quote-form',
+  templateUrl: './quote-form.component.html',
+  styleUrls: ['./quote-form.component.css']
+})
+export class QuoteFormComponent implements OnInit {
+
+  freshQuote = new Quotez(0,"","",0,0);
+
+  @Output() addQuote = new EventEmitter<Quotez>();
+
+
+  submitQuote(){
+    this.addQuote.emit(this.freshQuote)
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
