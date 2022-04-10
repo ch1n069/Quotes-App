15cf321fead415c111bb217eb1fc69e5812ce9b7
Moringa School <brunolalpachino@gmail.com>
2022-04-10 08:47:58 +0300

    hard coded afew codes

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotez } from '../quotez';
 
 @Component({
   selector: 'app-quotes',
@@ -8,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class QuotesComponent implements OnInit {
 
   public quotes: Quotez [] = [
-    new Quotez(1,)
+    new Quotez(1,"Take a deep breath, pick yourself up, dust yourself off, and start all over again.","Frank Santra",0,0),
+    new Quotez(2,"Take a deep breath, pick yourself up, dust yourself off, and start all over again.","Frank Santra",0,0),
+    new Quotez(3,"You will fall many times in life, but you will pick yourself up and become stronger","Leon Brown",0,0),
+    new Quotez(4,"To help yourself, you must be yourself. Be the best that you can be.","Dave peltzer",0,0),
+    
   ];
 
   constructor() { }
